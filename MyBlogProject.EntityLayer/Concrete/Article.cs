10635be6b7a5cc216a085930b64d908e280ef83c
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2024-10-25 21:56:57 +0300

    Add project files.

---
+++ MyBlogProject.EntityLayer/Concrete/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogProject.EntityLayer.Concrete
+{
+    public  class Article
+    {
+        public int ArticleId { get; set; }
+        public string Title { get; set; }
+        //public int CategoryId { get; set; }
+        //public Category Category { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CoverImageUrl { get; set; }
+        public string MainImageUrl { get; set; }
+
+
+    }
+}
---
+++ MyBlogProject.EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogProject.EntityLayer.Concrete
+{
+    public class Comment
+    {
+    }
+}

c45d58d59a7a584d14f6632606caeb8b88b90346
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2024-11-07 22:09:25 +0300

    Business  service and manager added

---
+++ MyBlogProject.BusinessLayer/Abstract/ITagCloudService.cs
@@ -0,0 +1,14 @@
+﻿using MyBlogProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogProject.BusinessLayer.Abstract
+{
+    public interface ITagCloudService:IGenericService<TagCloud>
+    {
+
+    }
+}
---
+++ MyBlogProject.BusinessLayer/Concrete/ArticleManager.cs
@@ -0,0 +1,46 @@
+﻿using MyBlogProject.BusinessLayer.Abstract;
+using MyBlogProject.DataAccessLayer.Abstract;
+using MyBlogProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogProject.BusinessLayer.Concrete
+{
+    public class ArticleManager : IArticleService
+    {
+        private readonly IArticleDal _articleDal;
+
+        public ArticleManager(IArticleDal articleDal)
+        {
+            _articleDal = articleDal;
+        }
+
+        public void TDelete(int id)
+        {
+           _articleDal.Delete(id);
+        }
+
+        public List<Article> TGetAll()
+        {
+            return _articleDal.GetAll();
+        }
+
+        public Article TGetById(int id)
+        {
+           return _articleDal.GetById(id);
+        }
+
+        public void TInsert(Article entity)
+        {
+           _articleDal.Insert(entity);
+        }
+
+        public void TUpdate(Article entity)
+        {
+          _articleDal.Update(entity);   
+        }
+    }
+}

905d8fdca7ffeef390bdd4bc37798353122a6c75
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2024-11-07 22:11:25 +0300

    Category added

---
+++ MyBlogProject.PresentationLayer/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlogProject.BusinessLayer.Abstract;
+
+namespace MyBlogProject.PresentationLayer.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult CategoryList()
+        {
+            var values=_categoryService.TGetAll();
+            return View(values);
+        }
+    }
+}
